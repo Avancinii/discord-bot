5c160ba8829a10a385ab57fffb2e1f077413d409
Avancinii <gustavo_avancini100997@outlook.com>
2024-04-16 13:37:05 -0300

    implementação do embed no slash commands

--- src/events/main/slash.ts
+++ src/events/main/slash.ts
@@ -1,4 +1,4 @@
-import { CommandInteractionOptionResolver } from "discord.js";
+import { CommandInteractionOptionResolver, EmbedBuilder  } from "discord.js";
 import { client } from "../..";
 import { Event } from "../../structs/types/Event";
 
@@ -13,6 +13,27 @@ export default new Event({
 
         const options = interaction.options as CommandInteractionOptionResolver;
 
+        const exampleEmbed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle('Some title')
+            .setURL('https://discord.js.org/')
+            .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+            .setDescription('Some description here')
+            .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+            .addFields(
+                { name: 'Regular field title', value: 'Some value here' },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+                { name: 'Inline field title', value: 'Some value here', inline: true },                                             
+            )
+            .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+            .setImage('https://i.imgur.com/AfFp7pu.png')
+            .setTimestamp()
+            .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+        interaction.reply({ embeds: [exampleEmbed] })
+        
+        
         command.run({client, interaction, options});
     }
 })
