0d0a14a8cd47c51c9a99263facd025a63c835441
Avancinii <gustavo_avancini100997@outlook.com>
2024-04-16 13:40:26 -0300

    edit do embed

--- src/events/main/slash.ts
+++ src/events/main/slash.ts
@@ -2,6 +2,55 @@ import { CommandInteractionOptionResolver, EmbedBuilder  } from "discord.js";
 import { client } from "../..";
 import { Event } from "../../structs/types/Event";
 
+const editExample = {
+	color: 0x0099ff,
+	title: 'Some title',
+	url: 'https://discord.js.org',
+	author: {
+		name: 'Some name',
+		icon_url: 'https://i.imgur.com/AfFp7pu.png',
+		url: 'https://discord.js.org',
+	},
+	description: 'Some description here',
+	thumbnail: {
+		url: 'https://i.imgur.com/AfFp7pu.png',
+	},
+	fields: [
+		{
+			name: 'Regular field title',
+			value: 'Some value here',
+		},
+		{
+			name: '\u200b',
+			value: '\u200b',
+			inline: false,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+	],
+	image: {
+		url: 'https://i.imgur.com/AfFp7pu.png',
+	},
+	timestamp: new Date().toISOString(),
+	footer: {
+		text: 'Some footer text here',
+		icon_url: 'https://i.imgur.com/AfFp7pu.png',
+	},
+};
+
 export default new Event({
     name: "interactionCreate",
     run(interaction){
@@ -32,7 +81,8 @@ export default new Event({
             .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
         interaction.reply({ embeds: [exampleEmbed] })
-        
+
+        interaction.editReply({ embeds: [editExample] })
         
         command.run({client, interaction, options});
     }
